e6b1b6125373a61ea830c09f148a948776f4accf
nghiemxuan <nghiemxuan2006@gmail.com>
2025-05-29 23:56:33 +0700

    add logo

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -1,7 +1,9 @@
 import ExpandRightIcon from './svg/expand-right-icon.svg';
 import ExpandLeftIcon from './svg/expand-left-icon.svg';
+import ViteIcon from './svg/vite-icon.svg';
 
-export default {
+export {
     ExpandRightIcon,
     ExpandLeftIcon,
-}
+    ViteIcon
+};
--- src/layout/SideBar.tsx
+++ src/layout/SideBar.tsx
@@ -9,32 +9,43 @@ import {
     faUser
 } from "@fortawesome/free-solid-svg-icons";
 import './SideBar.css'; // Assuming you have a CSS file for styling
+import { ExpandLeftIcon, ExpandRightIcon, ViteIcon } from '../assets/icons';
 const SideBar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     const handleTrigger = () => setIsOpen(!isOpen);
     return (
         <div className={`sidebar ${isOpen ? "sidebar--open" : ""}`}>
-            <div className="trigger" onClick={handleTrigger}>
-                <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+            <div className='sidebar-header'>
+                <div className="trigger" onClick={handleTrigger}>
+                    <img src={`${isOpen ? ExpandLeftIcon : ExpandRightIcon}`} alt="Toggle Sidebar" />
+
+                </div>
+                <img src={ViteIcon} alt="Logo" />
+                <span>Video Stream</span>
+            </div>
+            <div className='sidebar-content'>
+                <div className="sidebar-position">
+                    <FontAwesomeIcon icon={faUser} />
+                    <span>Home</span>
+                </div>
+                <div className="sidebar-position">
+                    <FontAwesomeIcon icon={faCogs} />
+                    <span>Menu item 2</span>
+                </div>
+                <div className="sidebar-position">
+                    <FontAwesomeIcon icon={faTable} />
+                    <span>Menu item 3</span>
+                </div>
+
+                <div className="sidebar-position">
+                    <FontAwesomeIcon icon={faList} />
+                    <span>Position 4</span>
+                </div>
             </div>
 
-            <div className="sidebar-position">
-                <FontAwesomeIcon icon={faUser} />
-                <span>Home</span>
-            </div>
-            <div className="sidebar-position">
-                <FontAwesomeIcon icon={faCogs} />
-                <span>Menu item 2</span>
-            </div>
-            <div className="sidebar-position">
-                <FontAwesomeIcon icon={faTable} />
-                <span>Menu item 3</span>
-            </div>
+            <div className="sidebar-footer">
 
-            <div className="sidebar-position">
-                <FontAwesomeIcon icon={faList} />
-                <span>Position 4</span>
             </div>
         </div>
     )
